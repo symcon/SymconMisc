b252aabae3f2e6a3fc8fa1d744de27eee35ef1db
Michael Maroszek <mm@ip-symcon.de>
2015-03-25 15:38:46 +0100

    add new Geofency module

---
+++ Geofency/module.php
@@ -0,0 +1,119 @@
+<?
+
+	class Geofency extends IPSModule
+	{
+		
+		public function __construct($InstanceID)
+		{
+			//Never delete this line!
+			parent::__construct($InstanceID);
+			
+			//These lines are parsed on Symcon Startup or Instance creation
+			//You cannot use variables here. Just static values.
+			$this->RegisterPropertyString("Username", "");
+			$this->RegisterPropertyString("Password", "");
+		}
+	
+		public function ApplyChanges()
+		{
+			//Never delete this line!
+			parent::ApplyChanges();
+			
+			$sid = $this->RegisterScript("Hook", "Hook", "<? //Do not delete or modify.\ninclude(\"__ipsmodule.inc.php\");\ninclude(\"../modules/SymconMisc/Geofency/module.php\");\n(new Geofency(".$this->InstanceID."))->ProcessHookData();");
+			$this->RegisterHook("/hook/geofency", $sid);
+		}
+		
+		private function RegisterHook($Hook, $TargetID)
+		{
+			$ids = IPS_GetInstanceListByModuleID("{015A6EB8-D6E5-4B93-B496-0D3F77AE9FE1}");
+			if(sizeof($ids) > 0) {
+				$hooks = json_decode(IPS_GetProperty($ids[0], "Hooks"), true);
+				$found = false;
+				foreach($hooks as $index => $hook) {
+					if($hook['Hook'] == "/hook/geofency") {
+						if($hook['TargetID'] == $TargetID)
+							return;
+						$hooks[$index]['TargetID'] = $TargetID;
+						$found = true;
+					}
+				}
+				if(!$found) {
+					$hooks[] = Array("Hook" => "/hook/geofency", "TargetID" => $TargetID);
+				}
+				IPS_SetProperty($ids[0], "Hooks", json_encode($hooks));
+				IPS_ApplyChanges($ids[0]);
+			}
+		}
+	
+		/**
+		* This function will be available automatically after the module is imported with the module control.
+		* Using the custom prefix this function will be callable from PHP and JSON-RPC through:
+		*
+		* GEO_ProcessHookData($id);
+		*
+		*/
+		public function ProcessHookData()
+		{
+		
+			
+			if(!isset($_POST['device']) || !isset($_POST['id']) || !isset($_POST['name'])) {
+				IPS_LogMessage("Geofency", "Malformed data: ".print_r($_POST, true));
+			}
+			
+			$deviceID = $this->CreateInstanceByIdent($this->InstanceID, $this->ReduceGUIDToIdent($_POST['device']), "Device");
+			SetValue($this->CreateVariableByIdent($deviceID, "Latitude", "Latitude", 2), floatval($_POST['latitude']));
+			SetValue($this->CreateVariableByIdent($deviceID, "Longitude", "Longitude", 2), floatval($_POST['longitude']));
+			SetValue($this->CreateVariableByIdent($deviceID, "Timestamp", "Timestamp", 1, "~UnixTimestamp"), intval(strtotime($_POST['date'])));
+			SetValue($this->CreateVariableByIdent($deviceID, $this->ReduceGUIDToIdent($_POST['id']), $_POST['name'], 0, "~Presence"), intval($_POST['entry']) > 0);
+			
+		}
+		
+		private function ReduceGUIDToIdent($guid) {
+			return str_replace(Array("{", "-", "}"), "", $guid);
+		}
+		
+		private function CreateCategoryByIdent($id, $ident, $name)
+		 {
+			 $cid = @IPS_GetObjectIDByIdent($ident, $id);
+			 if($cid === false)
+			 {
+				 $cid = IPS_CreateCategory();
+				 IPS_SetParent($cid, $id);
+				 IPS_SetName($cid, $name);
+				 IPS_SetIdent($cid, $ident);
+			 }
+			 return $cid;
+		}
+		
+		private function CreateVariableByIdent($id, $ident, $name, $type, $profile = "")
+		 {
+			 $vid = @IPS_GetObjectIDByIdent($ident, $id);
+			 if($vid === false)
+			 {
+				 $vid = IPS_CreateVariable($type);
+				 IPS_SetParent($vid, $id);
+				 IPS_SetName($vid, $name);
+				 IPS_SetIdent($vid, $ident);
+				 if($profile != "")
+					IPS_SetVariableCustomProfile($vid, $profile);
+			 }
+			 return $vid;
+		}
+		
+		private function CreateInstanceByIdent($id, $ident, $name, $moduleid = "{485D0419-BE97-4548-AA9C-C083EB82E61E}")
+		 {
+			 $iid = @IPS_GetObjectIDByIdent($ident, $id);
+			 if($iid === false)
+			 {
+				 $iid = IPS_CreateInstance($moduleid);
+				 IPS_SetParent($iid, $id);
+				 IPS_SetName($iid, $name);
+				 IPS_SetIdent($iid, $ident);
+			 }
+			 return $iid;
+		}
+		
+	
+	}
+
+?>

ef9159b279791fec6ecf4596c8d4522b7a04360a
Michael Maroszek <maroszek@gmx.net>
2016-06-14 17:07:09 +0200

    refactor AnwesenheitsSimulation for better timer logic

--- AnwesenheitsSimulation/module.php
+++ AnwesenheitsSimulation/module.php
@@ -12,7 +12,7 @@ class AnwesenheitsSimulation extends IPSModule
 		$this->RegisterPropertyInteger("ArchiveControlID", IPS_GetInstanceListByModuleID("{43192F0B-135B-4CE7-A0A7-1475603F3060}")[0]);
 		
 		//Timer
-		$this->RegisterTimer("UpdateDataTimer", 0, 'AS_UpdateData($_IPS[\'TARGET\']);');
+		$this->RegisterMidnightTimer("UpdateDataTimer", 'if(AS_UpdateData($_IPS[\'TARGET\'])) {AS_UpdateTargets($_IPS[\'TARGET\']);}');
 		$this->RegisterTimer("UpdateTargetsTimer", 0, 'AS_UpdateTargets($_IPS[\'TARGET\']);');
 		
 		//Variables
@@ -44,13 +44,14 @@ class AnwesenheitsSimulation extends IPSModule
 			//When activating the simulation, fetch actual data for a day and activate timer for updating targets
 			if($this->UpdateData()) {
 				$this->UpdateTargets();
+				IPS_SetEventActive(IPS_GetObjectIDByIdent("UpdateDataTimer", $this->InstanceID), true);
 			}
 		} else {
 			//When deactivating the simulation, kill data for simulation and deactivate timer for updating targets
 			SetValue(IPS_GetObjectIDByIdent("SimulationDay", $this->InstanceID), "Simulation deaktiviert");
 			SetValue(IPS_GetObjectIDByIdent("SimulationData", $this->InstanceID), "");
 			$this->SetTimerInterval("UpdateTargetsTimer", 0);
-			$this->SetTimerInterval("UpdateDataTimer", 0);
+			IPS_SetEventActive(IPS_GetObjectIDByIdent("UpdateDataTimer", $this->InstanceID), false);
 		}
 		
 		SetValue($this->GetIDForIdent("Active"), $SwitchOn);
@@ -167,19 +168,16 @@ class AnwesenheitsSimulation extends IPSModule
 			SetValue(IPS_GetObjectIDByIdent("SimulationDay", $this->InstanceID), $simulationData['Date']);
 			SetValue(IPS_GetObjectIDByIdent("SimulationData", $this->InstanceID), wddx_serialize_value($simulationData['Data']));
 		}
-
-		//Set timer for fetching data after midnight at 00:00:01
-		$this->SetTimerInterval("UpdateDataTimer", (strtotime("tomorrow") - time() + 1) * 1000);
-
+		
 		return sizeof($simulationData) > 0;
 		
 	}
 
-	public function UpdateTargets() {
-	
-		$simulationData = wddx_deserialize(GetValueString(IPS_GetObjectIDByIdent("SimulationData", $this->InstanceID)));
+	public function GetNextSimulationData() {
 		
+		$simulationData = wddx_deserialize(GetValueString(IPS_GetObjectIDByIdent("SimulationData", $this->InstanceID)));
 		$nextSwitchTimestamp = PHP_INT_MAX;
+		$result = array();
 		
 		//Being sure there is simulationData
 		if($simulationData !== NULL && $simulationData != "") {
@@ -199,45 +197,72 @@ class AnwesenheitsSimulation extends IPSModule
 							break;
 						}
 					}
-
-					$v = IPS_GetVariable($id);
 					
-					if(!isset($varValue)) {
-						$this->SendDebug("Update", "Device ".$id." will not be simulated today", 0);
-					} else {
-						$this->SendDebug("Update", "Device ".$id." shall be ".$varValue." since ".$varTime." and current is ".$v["VariableValue"], 0);
+					if (isset($varValue)) {
+						$result[$id] = array("varValue" => $varValue, "varTime" => $varTime);
 					}
-
-					//Set variableValue, if there is a varValue and its not the same as already set
-					if (isset($varValue) && ($varValue != $v["VariableValue"])) {
-						$o = IPS_GetObject($id);
-						if($v['VariableCustomAction'] != "") {
-							$actionID = $v['VariableCustomAction'];
-						} else {
-							$actionID = $v['VariableAction'];
-						}
-						
-						$this->SendDebug("Action", "Device ".$id." will be updated!", 0);
-
-						if(IPS_InstanceExists($actionID)) {
-							IPS_RequestAction($actionID, $o['ObjectIdent'], $varValue);
-						} else if(IPS_ScriptExists($actionID)) {
-							echo IPS_RunScriptWaitEx($actionID, Array("VARIABLE" => $id, "VALUE" => $varValue));
-						}
-					}
+					
 				}
 			}
 		} else {
 			echo "No valid SimulationData";
 		}
 		
-		if( $nextSwitchTimestamp == PHP_INT_MAX) {
-			//Timer set off because no oncoming actorswitch
-			$this->SetTimerInterval("UpdateTargetsTimer", 0);
-		} else {
-			//Timer set on the next switch of an simulated actor
-			$this->SetTimerInterval("UpdateTargetsTimer", ($nextSwitchTimestamp - time() + 1) * 1000);
+		if($nextSwitchTimestamp != PHP_INT_MAX) {
+			$result["nextSwitchTimestamp"] = $nextSwitchTimestamp;
 		}
+		
+		return $result;
+	}
+	
+	
+	public function UpdateTargets() {
+
+		$targetIDs = $this->GetTargets();
+		
+		$NextSimulationData = $this->GetNextSimulationData();
+		
+		foreach ($targetIDs as $targetID){
+			
+			$v = IPS_GetVariable($targetID);
+			
+			if(!isset($NextSimulationData[$targetID])) {
+				$this->SendDebug("Update", "Device ".$targetID." has no simulation data for now!", 0);
+				
+				$targetValue = false;
+			} else {
+				$this->SendDebug("Update", "Device ".$targetID." shall be ".(int)$NextSimulationData[$targetID]['varValue']." since ".$NextSimulationData[$targetID]['varTime']." and currently is ".(int)$v["VariableValue"], 0);
+				
+				//Set variableValue, if there is a varValue and its not the same as already set
+				$targetValue = $NextSimulationData[$targetID]['varValue'];
+			}
+			
+			if ($targetValue != $v["VariableValue"]) {
+				
+				$o = IPS_GetObject($targetID);
+				if($v['VariableCustomAction'] != "") {
+					$actionID = $v['VariableCustomAction'];
+				} else {
+					$actionID = $v['VariableAction'];
+				}
+				
+				$this->SendDebug("Action", "Device ".$targetID." will be updated!", 0);
+				
+				if(IPS_InstanceExists($actionID)) {
+					IPS_RequestAction($actionID, $o['ObjectIdent'], $targetValue);
+				} else if(IPS_ScriptExists($actionID)) {
+					echo IPS_RunScriptWaitEx($actionID, Array("VARIABLE" => $targetID, "VALUE" => $targetValue));
+				}
+				
+			}
+		}
+		
+		if(isset($NextSimulationData['nextSwitchTimestamp'])) {
+			$this->SetTimerInterval("UpdateTargetsTimer", ($NextSimulationData['nextSwitchTimestamp'] - time() + 1) * 1000);
+		} else {
+			$this->SetTimerInterval("UpdateTargetsTimer", 0);
+		}
+		
 	
 	}
 
@@ -251,6 +276,34 @@ class AnwesenheitsSimulation extends IPSModule
 			 IPS_SetIdent($cid, $ident);
 		 }
 		 return $cid;
+	}
+
+	private function RegisterMidnightTimer($Ident, $Action) {
+		
+		//search for already available scripts with proper ident
+		$eid = @IPS_GetObjectIDByIdent($Ident, $this->InstanceID);
+		
+		//properly update eventID
+		if($eid === false) {
+			$eid = 0;
+		} else if(IPS_GetEvent($eid)['EventType'] <> 1) {
+			IPS_DeleteEvent($eid);
+			$eid = 0;
+		}
+		
+		//we need to create one
+		if ($eid == 0) {
+			$eid = IPS_CreateEvent(1);
+			IPS_SetParent($eid, $this->InstanceID);
+			IPS_SetIdent($eid, $Ident);
+			IPS_SetName($eid, $Ident);
+			IPS_SetHidden($eid, true);
+			IPS_SetEventScript($eid, $Action);
+		}
+		
+		IPS_SetEventCyclic($eid, 2, 1, 0, 0, 0, 0);
+		IPS_SetEventCyclicTimeFrom($eid, 0, 0, 1);
+	
 	}
 
 }

9e2abebe9e9652cd9655e627587a76fff1609f16
Michael Maroszek <maroszek@gmx.net>
2016-06-15 11:12:09 +0200

    add new module EgiGeoZone

---
+++ EgiGeoZone/README.md
@@ -0,0 +1,77 @@
+# EgiGeoZone
+Das Modul dient zum empfangen von EgiGeoZone Daten.
+
+### Inhaltverzeichnis
+
+1. [Funktionsumfang](#1-funktionsumfang)
+2. [Voraussetzungen](#2-voraussetzungen)
+3. [Software-Installation](#3-software-installation)
+4. [Einrichten der Instanzen in IP-Symcon](#4-einrichten-der-instanzen-in-ip-symcon)
+5. [Statusvariablen und Profile](#5-statusvariablen-und-profile)
+6. [WebFront](#6-webfront)
+7. [PHP-Befehlsreferenz](#7-php-befehlsreferenz)
+8. [Anhang](#8-anhang)
+
+
+### 1. Funktionsumfang
+
+* Pro Gerät eine eigene Standortliste
+* Benutzername und Passwort Identifikation innerhalb von IP-Symcon.
+* Richtet automatisch den Webhook "/hook/egigeozone" ein.
+ * Es wird empfohlen dies in Kombination mit dem Connect Modul zu nutzen.
+
+![](EgiGeoZone-InAppConfig.png)
+
+### 2. Voraussetzungen
+
+- IP-Symcon ab Version 4.x
+- EgiGeoZone App für Android OS
+
+### 3. Software-Installation
+
+Über das Modul-Control folgende URL hinzufügen.
+`git://github.com/paresy/SymconMisc.git`
+
+### 4. Einrichten der Instanzen in IP-Symcon
+
+- Unter "Instanz hinzufügen" ist das 'EgiGeoZone'-Modul unter dem Hersteller '(Sonstige)' aufgeführt.
+
+__Konfigurationsseite__:
+
+Name         | Beschreibung
+------------ | ---------------------------------
+Benutzername | Benutzername, welcher in der EgiGeoZone App angegeben werden muss, um IP-Symcon Daten schicken zu können.
+Passwort     | Passwort, welches in der EgiGeoZone App angeben werden muss.
+
+_Werden diese Daten Leer gelassen kann jeder über den Hook Daten an IP-Symcon schicken._
+
+### 5. Statusvariablen und Profile
+
+Die Statusvariablen/Kategorien werden automatisch angelegt. Das Löschen einzelner kann zu Fehlfunktionen führen.
+
+##### Statusvariablen
+Die Variablen werden anhand der Geräte ID und beim erstmaligen senden innerhalb des EgiGeoZone Moduls automatisch angelegt. Es können mehrere Geräte über einen Hook laufen. Jedes Gerät wird unter seiner eigenen "Kategorie" eingerichtet.
+
+Name                           | Typ             | Beschreibung
+------------------------------ | --------------- | ----------------
+Gerätename                     | Instanz (Dummy) | Dient als "Kategorie" in der sich alle überwachten Standorte, sowie der Zeitstempel und Längen-/Breitengrad befinden. Wird pro Gerät erstellt.
+Latitude                       | Float           | Breitengrad der letzten Aktivität.
+Longitude                      | Float           | Längengrad der letzten Aktivität.
+Timestamp                      | Integer         | UnixTimestamp der letzten Aktivität.
+Beispielstandort (GeoZoneTest) | Boolean         | Present oder Absent. Information wird von Gefency geliefert.
+
+Beispiel:
+![](EgiGeoZone-Variablen.png)
+
+##### Profile:
+
+Es werden keine zusätzlichen Profile hinzugefügt
+
+### 6. WebFront
+
+Es gibt keine native Darstellung via WebFront oder in den mobilen Apps.
+Geräte und Variablen, welche angezeigt werden sollen, können via Link angezeigt werden.
+
+### 7. PHP-Befehlsreferenz
+
+Es sind keine PHP-Skriptbefehle vorhanden.
---
+++ EgiGeoZone/module.php
@@ -0,0 +1,135 @@
+<?
+
+	class EgiGeoZone extends IPSModule
+	{
+		
+		public function Create()
+		{
+			//Never delete this line!
+			parent::Create();
+			
+			$this->RegisterPropertyString("Username", "");
+			$this->RegisterPropertyString("Password", "");
+		}
+	
+		public function ApplyChanges()
+		{
+			//Never delete this line!
+			parent::ApplyChanges();
+			
+			$sid = $this->RegisterScript("Hook", "Hook", "<? //Do not delete or modify.\ninclude(IPS_GetKernelDirEx().\"scripts/__ipsmodule.inc.php\");\ninclude(\"../modules/SymconEGZ/EgiGeoZone/module.php\");\n(new EgiGeoZone(".$this->InstanceID."))->ProcessHookData();");
+			$this->RegisterHook("/hook/egigeozone", $sid);
+		}
+		
+		private function RegisterHook($Hook, $TargetID)
+		{
+			$ids = IPS_GetInstanceListByModuleID("{015A6EB8-D6E5-4B93-B496-0D3F77AE9FE1}");
+			if(sizeof($ids) > 0) {
+				$hooks = json_decode(IPS_GetProperty($ids[0], "Hooks"), true);
+				$found = false;
+				foreach($hooks as $index => $hook) {
+					if($hook['Hook'] == "/hook/egigeozone") {
+						if($hook['TargetID'] == $TargetID)
+							return;
+						$hooks[$index]['TargetID'] = $TargetID;
+						$found = true;
+					}
+				}
+				if(!$found) {
+					$hooks[] = Array("Hook" => "/hook/egigeozone", "TargetID" => $TargetID);
+				}
+				IPS_SetProperty($ids[0], "Hooks", json_encode($hooks));
+				IPS_ApplyChanges($ids[0]);
+			}
+		}
+	
+		/**
+		* This function will be available automatically after the module is imported with the module control.
+		* Using the custom prefix this function will be callable from PHP and JSON-RPC through:
+		*
+		* GEO_ProcessHookData($id);
+		*
+		*/
+		public function ProcessHookData()
+		{
+			if($_IPS['SENDER'] == "Execute") {
+				echo "This script cannot be used this way.";
+				return;
+			}
+			
+			if((IPS_GetProperty($this->InstanceID, "Username") != "") || (IPS_GetProperty($this->InstanceID, "Password") != "")) {
+				if(!isset($_SERVER['PHP_AUTH_USER']))
+					$_SERVER['PHP_AUTH_USER'] = "";
+				if(!isset($_SERVER['PHP_AUTH_PW']))
+					$_SERVER['PHP_AUTH_PW'] = "";
+					
+				if(($_SERVER['PHP_AUTH_USER'] != IPS_GetProperty($this->InstanceID, "Username")) || ($_SERVER['PHP_AUTH_PW'] != IPS_GetProperty($this->InstanceID, "Password"))) {
+					header('WWW-Authenticate: Basic Realm="Geofency WebHook"');
+					header('HTTP/1.0 401 Unauthorized');
+					echo "Authorization required";
+					return;
+				}
+			}
+			
+			if(!isset($_GET['device']) || !isset($_GET['id']) || !isset($_GET['name'])) {
+				IPS_LogMessage("EgiGeoZone", "Malformed data: ".print_r($_GET, true));
+				return;
+			}
+			
+			$deviceID = $this->CreateInstanceByIdent($this->InstanceID, $this->ReduceGUIDToIdent($_GET['device']), "Device");
+			SetValue($this->CreateVariableByIdent($deviceID, "Latitude", "Latitude", 2), floatval($_GET['latitude']));
+			SetValue($this->CreateVariableByIdent($deviceID, "Longitude", "Longitude", 2), floatval($_GET['longitude']));
+			SetValue($this->CreateVariableByIdent($deviceID, "Timestamp", "Timestamp", 1, "~UnixTimestamp"), intval(strtotime($_GET['date'])));
+			SetValue($this->CreateVariableByIdent($deviceID, $this->ReduceGUIDToIdent($_GET['id']), utf8_decode($_GET['name']), 0, "~Presence"), intval($_GET['entry']) > 0);
+			
+		}
+		
+		private function ReduceGUIDToIdent($guid) {
+			return str_replace(Array("{", "-", "}"), "", $guid);
+		}
+		
+		private function CreateCategoryByIdent($id, $ident, $name)
+		 {
+			 $cid = @IPS_GetObjectIDByIdent($ident, $id);
+			 if($cid === false)
+			 {
+				 $cid = IPS_CreateCategory();
+				 IPS_SetParent($cid, $id);
+				 IPS_SetName($cid, $name);
+				 IPS_SetIdent($cid, $ident);
+			 }
+			 return $cid;
+		}
+		
+		private function CreateVariableByIdent($id, $ident, $name, $type, $profile = "")
+		 {
+			 $vid = @IPS_GetObjectIDByIdent($ident, $id);
+			 if($vid === false)
+			 {
+				 $vid = IPS_CreateVariable($type);
+				 IPS_SetParent($vid, $id);
+				 IPS_SetName($vid, $name);
+				 IPS_SetIdent($vid, $ident);
+				 if($profile != "")
+					IPS_SetVariableCustomProfile($vid, $profile);
+			 }
+			 return $vid;
+		}
+		
+		private function CreateInstanceByIdent($id, $ident, $name, $moduleid = "{485D0419-BE97-4548-AA9C-C083EB82E61E}")
+		 {
+			 $iid = @IPS_GetObjectIDByIdent($ident, $id);
+			 if($iid === false)
+			 {
+				 $iid = IPS_CreateInstance($moduleid);
+				 IPS_SetParent($iid, $id);
+				 IPS_SetName($iid, $name);
+				 IPS_SetIdent($iid, $ident);
+			 }
+			 return $iid;
+		}
+		
+	
+	}
+
+?>

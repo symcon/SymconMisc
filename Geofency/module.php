88ca8d791876f025f6f273c47219568769ac5c2b
Michael Maroszek <maroszek@gmx.net>
2016-12-14 13:27:26 +0100

    Revert "remove temporary script which IP-Symcon 4.2 does not need anymore"
    This reverts commit ed83ac0b761fa6f9141df18a665169f3aa6a5538.

--- Geofency/module.php
+++ Geofency/module.php
@@ -14,24 +14,25 @@
 			//Never delete this line!
 			parent::ApplyChanges();
 			
-			$this->RegisterHook("/hook/geofency");
+			$sid = $this->RegisterScript("Hook", "Hook", "<? //Do not delete or modify.\ninclude(IPS_GetKernelDirEx().\"scripts/__ipsmodule.inc.php\");\ninclude(\"../modules/SymconMisc/Geofency/module.php\");\n(new Geofency(".$this->InstanceID."))->ProcessHookData();");
+			$this->RegisterHook("/hook/geofency", $sid);
 		}
 		
-		private function RegisterHook($WebHook) {
+		private function RegisterHook($Hook, $TargetID) {
 			$ids = IPS_GetInstanceListByModuleID("{015A6EB8-D6E5-4B93-B496-0D3F77AE9FE1}");
 			if(sizeof($ids) > 0) {
 				$hooks = json_decode(IPS_GetProperty($ids[0], "Hooks"), true);
 				$found = false;
 				foreach($hooks as $index => $hook) {
-					if($hook['Hook'] == $WebHook) {
-						if($hook['TargetID'] == $this->InstanceID)
+					if($hook['Hook'] == "/hook/geofency") {
+						if($hook['TargetID'] == $TargetID)
 							return;
-						$hooks[$index]['TargetID'] = $this->InstanceID;
+						$hooks[$index]['TargetID'] = $TargetID;
 						$found = true;
 					}
 				}
 				if(!$found) {
-					$hooks[] = Array("Hook" => $WebHook, "TargetID" => $this->InstanceID);
+					$hooks[] = Array("Hook" => "/hook/geofency", "TargetID" => $TargetID);
 				}
 				IPS_SetProperty($ids[0], "Hooks", json_encode($hooks));
 				IPS_ApplyChanges($ids[0]);
@@ -39,9 +40,13 @@
 		}
 	
 		/**
-		* This function will be called by the hook control. Visibility should be protected!
+		* This function will be available automatically after the module is imported with the module control.
+		* Using the custom prefix this function will be callable from PHP and JSON-RPC through:
+		*
+		* GEO_ProcessHookData($id);
+		*
 		*/
-		protected function ProcessHookData() {
+		public function ProcessHookData() {
 			if($_IPS['SENDER'] == "Execute") {
 				echo "This script cannot be used this way.";
 				return;
@@ -62,7 +67,7 @@
 			}
 			
 			if(!isset($_POST['device']) || !isset($_POST['id']) || !isset($_POST['name'])) {
-				$this->SendDebug("Geofency", "Malformed data: ".print_r($_POST, true), 0);
+				IPS_LogMessage("Geofency", "Malformed data: ".print_r($_POST, true));
 				return;
 			}
 			

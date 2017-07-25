c198f59eb160655289d62a27a1236a80cb63ce44
Michael Maroszek <maroszek@gmx.net>
2017-07-25 16:29:37 +0200

    update WasserAlarm module

--- WasserAlarm/README.md
+++ WasserAlarm/README.md
@@ -1,8 +1,9 @@
 # WasserAlarm
 Das Modul dient dazu einen unnatürlich hohen Wasserverbrauch festzustellen. Es reagiert auf eine Zählervariable und schaltet unter bestimmten Bedingungen einen Alarm.
 Es gibt zwei Alarmvariablen.  
-Einen Durchfluss-Alarm, welcher schaltet wenn große Mengen auf einmal fließen (z.B. Rohrbruch).
-Einen Alarmlevel, welcher in 7 Stufen hochtickt wenn über längeren Zeitraum eine kleine Menge fließt (z.B. tropfender Wasserhahn)
+Einen Rohrbruch-Alarm, welcher schaltet wenn große Mengen auf einmal fließen.
+Einen Leckage-Alarm, welcher in 7 Stufen hochtickt wenn über längeren Zeitraum eine kleine Menge fließt (z.B. tropfender Wasserhahn).
+Das Intervall für beide Kontrollen
 
 ### Inhaltverzeichnis
 
@@ -17,9 +18,10 @@ Einen Alarmlevel, welcher in 7 Stufen hochtickt wenn über längeren Zeitraum ei
 ### 1. Funktionsumfang
 
 * Auswahl der Wasserzählervariable
-* Durchfluss Timer in Minuten
-* 7 Stufen für Alarmlevel des kleinen Wasserverlustes
-* Alarmanzeige bei Durchfluss
+* Rohrburch Timer in Minuten einstellbar
+* Leckage Timer in Minuten einstellbar
+* 7 Stufen Anzeige für Leckage
+* Alarmanzeige bei Rohrbruch
 
 ### 2. Voraussetzungen
 
@@ -36,11 +38,11 @@ Einen Alarmlevel, welcher in 7 Stufen hochtickt wenn über längeren Zeitraum ei
 
 __Konfigurationsseite__:
 
-Name           | Beschreibung
--------------- | ---------------------------------
-Zählervariable | Variable, welche den Zählerwert wiedergibt.
-Intervall      | Zeitintervall in dem kontrolliert wird, ob zuviel Wasser durchgeflossen ist und möglicherweise ein Rohrbruch vorliegt. Misst das Verhältnis zum "Grenzwert Durchfluss"
-
+Name                | Beschreibung
+------------------- | ---------------------------------
+Zählervariable      | Variable, welche den Zählerwert wiedergibt.
+Leckage Intervall   | __Standard: 1min__ Zeitintervall in dem kontrolliert wird, ob zuviel Wasser durchgeflossen ist.
+Rohrbruch Intervall | __Standard: 15min__ Zeitintervall in dem kontrolliert wird, ob zuviel Wasser durchgeflossen ist.
 
 ### 5. Statusvariablen und Profile
 
@@ -48,21 +50,19 @@ Die Statusvariablen/Kategorien werden automatisch angelegt. Das Löschen einzeln
 
 ##### Statusvariablen
 
-Name               | Typ       | Beschreibung
------------------- | --------- | ----------------
-CountAlertBorder   | Float     | Grenzwert, welcher jede Minute überprüft wird und falls dies der Fall ist um 1 erhöht wird. Wenn er unterhalb der Grenze liegt, wird das Alarmlevel resetet. 
-CounterAlertStatus | Integer   | 7 Stufenanzeige für den Stand den Alarmlevels.
-CounterValueOld    | Float     | Alter Wert, welcher zum Vergleich genutzt wird.
-FlowAlertBorder    | Float     | Grenzwert, welcher geprüft wird und gegebenfalls der Auslöser für einen Alarm ist. 
-FlowAlertStatus    | Boolean   | Alarm ob der Durchfluss zu hoch ist. 
-FlowValueOld       | Float     | Alter Wert, welcher zum Vergleich genutzt wird.
+Name                | Typ       | Beschreibung
+------------------- | --------- | ----------------
+Leckage Grenzwert   | Float     | Differenzwert, welcher im eingestellten "Leckage Intervall" überprüft wird und bei Überschreitung wird die "Leckage" um 1 erhöht. Wenn die Differenz unterhalb oder gleich der Grenze ist, wird das Alarmlevel resetet. 
+Leckage             | Integer   | 7 Stufenanzeige für den Stand den Alarmlevels.
+Rohrbruch Grenzwert | Float     | Grenzwert, welcher geprüft wird und gegebenfalls der Auslöser für einen Alarm ist. 
+Rohrbruch           | Boolean   | Alarm ob der Durchfluss zu hoch ist. 
 
 ##### Profile:
 
-Bezeichnung     | Beschreibung
---------------- | -----------------
-WAA.AlertLevel  | Profil für CountAlertStatus - 7 Alarmstufen mit verschiedenen Symbolen und Farbanzeigen
-WAA.BorderValue | Profil für Flow-/CountAlertBorder
+Bezeichnung        | Beschreibung
+------------------ | -----------------
+WAA.LeakLevel      | Profil für Leckage - 7 Alarmstufen mit verschiedenen Symbolen und Farbanzeigen
+WAA.ThresholdValue | Profil für Leckage/Rohrbruch Grenzwert
 
 ### 6. WebFront
 
@@ -74,4 +74,4 @@ Es wird zusätzlich angezeigt, ob ein Alarm vorliegt oder nicht.
 `boolean WAA_CheckAlert(integer $InstanzID, String $BorderValue, String $OldValue);`
 Kontrolliert innerhalb des WasserAlarms mit der InstanzID $InstanzID ob ein Grenzwert überschritten wurde und setzt die Alarmvariablen  
 Die Funktion liefert keinerlei Rückgabewert.  
-`WAA_CheckAlert(12345, "CountAlertBorder", "CounterValueOld");``
+`WAA_CheckAlert(12345, "LeakThreashold", "LeakBuffer");``
--- WasserAlarm/module.php
+++ WasserAlarm/module.php
@@ -7,43 +7,43 @@ class WasserAlarm extends IPSModule {
 		
 		//These lines are parsed on Symcon Startup or Instance creation
 		//You cannot use variables here. Just static values.
-		$this->RegisterPropertyInteger("CounterID", 0);
-		$this->RegisterPropertyInteger("Interval", 15);
+		$this->RegisterPropertyInteger("MeterID", 0);
+		$this->RegisterPropertyInteger("LeakInterval", 1);
+		$this->RegisterPropertyInteger("PipeBurstInterval", 15);
 		
 		//Timer
-		$this->RegisterTimer("UpdateCounter", 0, 'WAA_CheckAlert($_IPS[\'TARGET\'], "CountAlertBorder", "CounterValueOld");');
-		$this->RegisterTimer("UpdateFlow", 0, 'WAA_CheckAlert($_IPS[\'TARGET\'], "FlowAlertBorder", "FlowValueOld");');
+		$this->RegisterTimer("UpdateLeak", 0, 'WAA_CheckAlert($_IPS[\'TARGET\'], "LeakThreshold", "LeakBuffer");');
+		$this->RegisterTimer("UpdatePipeBurst", 0, 'WAA_CheckAlert($_IPS[\'TARGET\'], "PipeBurstThreshold", "PipeBurstBuffer");');
 		
 		//Variablenprofile
 		//AlertLevel
-		if(!IPS_VariableProfileExists("WAA.AlertLevel")){
-			IPS_CreateVariableProfile("WAA.AlertLevel", 1);
-			IPS_SetVariableProfileValues("WAA.AlertLevel", 0, 6, 1);
-			IPS_SetVariableProfileAssociation("WAA.AlertLevel", 0, $this->Translate("No activity"), "IPS", 0x80FF80);
-			IPS_SetVariableProfileAssociation("WAA.AlertLevel", 1, $this->Translate("Everything fine"), "HollowArrowUp", 0x00FF00);
-			IPS_SetVariableProfileAssociation("WAA.AlertLevel", 2, $this->Translate("Normal activity"), "HollowDoubleArrowUp", 0x008000);
-			IPS_SetVariableProfileAssociation("WAA.AlertLevel", 3, $this->Translate("High activity"), "Lightning", 0xFFFF00);
-			IPS_SetVariableProfileAssociation("WAA.AlertLevel", 4, $this->Translate("Abnormal activity"), "Mail", 0xFF8040);
-			IPS_SetVariableProfileAssociation("WAA.AlertLevel", 5, $this->Translate("Pre-Alarm"), "Warning", 0xFF0000);
-			IPS_SetVariableProfileAssociation("WAA.AlertLevel", 6, $this->Translate("Alarm triggered"), "Alert", 0x800000);
+		if(!IPS_VariableProfileExists("WAA.LeakLevel")) {
+			IPS_CreateVariableProfile("WAA.LeakLevel", 1);
+			IPS_SetVariableProfileValues("WAA.LeakLevel", 0, 6, 1);
+			IPS_SetVariableProfileAssociation("WAA.LeakLevel", 0, $this->Translate("No activity"), "IPS", 0x80FF80);
+			IPS_SetVariableProfileAssociation("WAA.LeakLevel", 1, $this->Translate("Everything fine"), "HollowArrowUp", 0x00FF00);
+			IPS_SetVariableProfileAssociation("WAA.LeakLevel", 2, $this->Translate("Normal activity"), "HollowDoubleArrowUp", 0x008000);
+			IPS_SetVariableProfileAssociation("WAA.LeakLevel", 3, $this->Translate("High activity"), "Lightning", 0xFFFF00);
+			IPS_SetVariableProfileAssociation("WAA.LeakLevel", 4, $this->Translate("Abnormal activity"), "Mail", 0xFF8040);
+			IPS_SetVariableProfileAssociation("WAA.LeakLevel", 5, $this->Translate("Pre-Alarm"), "Warning", 0xFF0000);
+			IPS_SetVariableProfileAssociation("WAA.LeakLevel", 6, $this->Translate("Alarm triggered"), "Alert", 0x800000);
 		}
 		
 		//BorderValue
-		if(!IPS_VariableProfileExists("WAA.BorderValue")){
-			IPS_CreateVariableProfile("WAA.BorderValue", 2);
-			IPS_SetVariableProfileDigits("WAA.BorderValue", 3);
-			IPS_SetVariableProfileValues("WAA.BorderValue", 0, 250, 0.5);
+		if(!IPS_VariableProfileExists("WAA.ThresholdValue")) {
+			IPS_CreateVariableProfile("WAA.ThresholdValue", 2);
+			IPS_SetVariableProfileIcon("WAA.ThresholdValue",  "Distance");
+			IPS_SetVariableProfileDigits("WAA.ThresholdValue", 1);
+			IPS_SetVariableProfileValues("WAA.ThresholdValue", 0, 250, 0.5);
 		}
 		
-		$this->RegisterVariableInteger("CounterAlertStatus", $this->Translate("Alertlevel Counter (every minute)"), "WAA.AlertLevel");
-		$this->RegisterVariableFloat("CounterValueOld", $this->Translate("Old countervalue"), "");
-		$this->RegisterVariableFloat("CountAlertBorder", $this->Translate("Bordlervalue counteralert"), "WAA.BorderValue");
-		$this->EnableAction("CountAlertBorder");
+		$this->RegisterVariableInteger("Leak", $this->Translate("Leak"), "WAA.LeakLevel");
+		$this->RegisterVariableFloat("LeakThreshold", $this->Translate("Leak threshold"), "WAA.ThresholdValue");
+		$this->EnableAction("LeakThreshold");
 		
-		$this->RegisterVariableBoolean("FlowAlertStatus", $this->Translate("Alertstatus flow"), "~Alert");
-		$this->RegisterVariableFloat("FlowValueOld", $this->Translate("Old flowvalue"), "");
-		$this->RegisterVariableFloat("FlowAlertBorder", $this->Translate("Bordervalue flow"), "WAA.BorderValue");
-		$this->EnableAction("FlowAlertBorder");
+		$this->RegisterVariableBoolean("PipeBurst", $this->Translate("Pipe burst"), "~Alert");
+		$this->RegisterVariableFloat("PipeBurstThreshold", $this->Translate("Pipe burst threshold"), "WAA.ThresholdValue");
+		$this->EnableAction("PipeBurstThreshold");
 		
 	}
 
@@ -57,40 +57,42 @@ class WasserAlarm extends IPSModule {
 		//Never delete this line!
 		parent::ApplyChanges();
 		
-		if ($this->ReadPropertyInteger("CounterID") != 0){
-			$CounterValue = GetValue($this->ReadPropertyInteger("CounterID"));
-			SetValue($this->GetIDForIdent("CounterValueOld"), $CounterValue);
-			SetValue($this->GetIDForIdent("FlowValueOld"), $CounterValue);
+		if ($this->ReadPropertyInteger("MeterID") != 0) {
+			$CounterValue = GetValue($this->ReadPropertyInteger("MeterID"));
+			$this->SetBuffer("LeakBuffer", $CounterValue);
+			$this->SetBuffer("PipeBurstBuffer", $CounterValue);
 		}
 		
-		$this->SetTimerInterval("UpdateCounter", 60*1000);
-		$this->SetTimerInterval("UpdateFlow", $this->ReadPropertyInteger("Interval")*60*1000);
+		$this->SetTimerInterval("UpdateLeak", $this->ReadPropertyInteger("LeakInterval") * 60 * 1000);
+		$this->SetTimerInterval("UpdatePipeBurst", $this->ReadPropertyInteger("PipeBurstInterval") * 60 * 1000);
 	}
 
-	public function CheckAlert(String $BorderValue, String $OldValue){
+	public function CheckAlert(String $ThresholdName, String $BufferName) {
 		
-		$CounterValue = GetValue($this->ReadPropertyInteger("CounterID"));
-		$ValueOld = GetValueFloat($this->GetIDForIdent($OldValue));
+		$MeterValue = GetValue($this->ReadPropertyInteger("MeterID"));
+		$ValueOld = $this->GetBuffer($BufferName);
 		
-		if (($CounterValue - $ValueOld) > GetValueFloat($this->GetIDForIdent($BorderValue))) {
-			if ($BorderValue == "CountAlertBorder") {
-				SetValue($this->GetIDForIdent("CounterAlertStatus"), GetValueInteger($this->GetIDForIdent("CounterAlertStatus")) + 1);
-				SetValue($this->GetIDForIdent($OldValue), $CounterValue);
-			} elseif (GetValueFloat($this->GetIDForIdent($BorderValue)) != 0) {
-				//Kontrolle ob es die erste Abfrage nach Systemneustart ist. Wenn ja, den Wert ignorieren.
-				if (time() > (IPS_GetKernelStartTime() + ($this->ReadPropertyInteger("Interval") * 60) + 30)) {
-					SetValue($this->GetIDForIdent("FlowAlertStatus"), true);
-				}
-				SetValue($this->GetIDForIdent($OldValue), $CounterValue);
+		// if Threshold is exceeded -> Set Alert
+		if (($MeterValue - $ValueOld) > GetValueFloat($this->GetIDForIdent($ThresholdName))) {
+			if ($ThresholdName == "LeakThreshold") {
+				SetValue($this->GetIDForIdent("Leak"), GetValueInteger($this->GetIDForIdent("Leak")) + 1);
+				$this->SetBuffer($BufferName, $MeterValue);
+			}
+			elseif (GetValueFloat($this->GetIDForIdent($ThresholdName)) != 0) {
+				SetValue($this->GetIDForIdent("PipeBurst"), true);
+				$this->SetBuffer($BufferName, $MeterValue);
 			}
 			
-		} else {
-			if ($BorderValue == "CountAlertBorder") {
-				SetValue($this->GetIDForIdent("CounterAlertStatus"), 0);
-				SetValue($this->GetIDForIdent($OldValue), $CounterValue);
-			} elseif (GetValueFloat($this->GetIDForIdent($BorderValue)) != 0) {
-				SetValue($this->GetIDForIdent("FlowAlertStatus"), false);
-				SetValue($this->GetIDForIdent($OldValue), $CounterValue);
+		} 
+		// if Threshold is not exceeded -> reset Alert
+		else {
+			if ($ThresholdName == "LeakThreshold") {
+				SetValue($this->GetIDForIdent("Leak"), 0);
+				$this->SetBuffer($BufferName, $MeterValue);
+			}
+			elseif (GetValueFloat($this->GetIDForIdent($ThresholdName)) != 0) {
+				SetValue($this->GetIDForIdent("PipeBurst"), false);
+				$this->SetBuffer($BufferName, $MeterValue);
 			}
 		}
 	}
@@ -98,11 +100,12 @@ class WasserAlarm extends IPSModule {
 	public function RequestAction($Ident, $Value) {
 		
 		switch($Ident) {
-			case "CountAlertBorder":
-			case "FlowAlertBorder";
+			case "LeakThreshold":
+			case "PipeBurstThreshold";
 				//Neuen Wert in die Statusvariable schreiben
 				SetValue($this->GetIDForIdent($Ident), $Value);
 				break;
+				
 			default:
 				throw new Exception($this->Translate("Invalid Ident"));
 		}
